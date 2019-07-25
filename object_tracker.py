b5c9cf048ee655367ccc004d5843671c179be00c
izolot <izolot>
2019-07-25 18:04:21 +0300

    change depricated methods & some syntax refactors

--- object_tracker.py
+++ object_tracker.py
@@ -1,27 +1,18 @@
-import datetime
-import os
-import random
 import sys
-import time
-
-import torch
-from PIL import Image
-from torch.autograd import Variable
-from torch.utils.data import DataLoader
-from torchvision import datasets, transforms
-
 import cv2
+
+from torchvision import transforms
 from models import *
 from sort import *
 from utils import *
 
 # load weights and set defaults
-config_path='config/yolov3.cfg'
-weights_path='config/yolov3.weights'
-class_path='config/coco.names'
-img_size=416
-conf_thres=0.8
-nms_thres=0.4
+config_path = 'config/yolov3.cfg'
+weights_path = 'config/yolov3.weights'
+class_path = 'config/coco.names'
+img_size = 416
+conf_thres = 0.8
+nms_thres = 0.4
 
 STATIC_DETECT = False
 
@@ -34,16 +25,18 @@ model.eval()
 classes = utils.load_classes(class_path)
 Tensor = torch.cuda.FloatTensor
 
+
 def detect_image(img):
     # scale and pad image
-    ratio = min(img_size/img.size[0], img_size/img.size[1])
+    ratio = min(img_size / img.size[0], img_size / img.size[1])
     imw = round(img.size[0] * ratio)
     imh = round(img.size[1] * ratio)
-    img_transforms = transforms.Compose([ transforms.Resize((imh, imw)),
-         transforms.Pad((max(int((imh-imw)/2),0), max(int((imw-imh)/2),0), max(int((imh-imw)/2),0), max(int((imw-imh)/2),0)),
-                        (128,128,128)),
-         transforms.ToTensor(),
-         ])
+    img_transforms = transforms.Compose([transforms.Resize((imh, imw)),
+                                         transforms.Pad((max(int((imh - imw) / 2), 0), max(int((imw - imh) / 2), 0),
+                                                         max(int((imh - imw) / 2), 0), max(int((imw - imh) / 2), 0)),
+                                                        (128, 128, 128)),
+                                         transforms.ToTensor(),
+                                         ])
     # convert image to Tensor
     image_tensor = img_transforms(img).float()
     image_tensor = image_tensor.unsqueeze_(0)
@@ -51,30 +44,34 @@ def detect_image(img):
     # run inference on the model and get detections
     with torch.no_grad():
         detections = model(input_img)
-        detections = utils.non_max_suppression(detections, 80, conf_thres, nms_thres)
+        detections = utils.non_max_suppression(
+            detections, 80, conf_thres, nms_thres)
     return detections[0]
+
 
 videopath = sys.argv[1]
 
-colors=[(255,0,0),(0,255,0),(0,0,255),(255,0,255),(128,0,0),(0,128,0),(0,0,128),(128,0,128),(128,128,0),(0,128,128)]
+colors = [(255, 0, 0), (0, 255, 0), (0, 0, 255), (255, 0, 255), (128, 0, 0), (0, 128, 0), (0, 0, 128), (128, 0, 128),
+          (128, 128, 0), (0, 128, 128)]
 
 vid = cv2.VideoCapture(videopath)
-mot_tracker = Sort() 
+mot_tracker = Sort()
 
-cv2.namedWindow('Stream',cv2.WINDOW_NORMAL)
-cv2.resizeWindow('Stream', (800,600))
+cv2.namedWindow('Stream', cv2.WINDOW_NORMAL)
+cv2.resizeWindow('Stream', (800, 600))
 
 fourcc = cv2.VideoWriter_fourcc(*'XVID')
-ret,frame=vid.read()
+ret, frame = vid.read()
 vw = frame.shape[1]
 vh = frame.shape[0]
-print ("Video size", vw,vh)
-outvideo = cv2.VideoWriter(videopath.replace(".mp4", "-det.mp4"),fourcc,20.0,(vw,vh))
+print("Video size", vw, vh)
+outvideo = cv2.VideoWriter(videopath.replace(
+    ".mp4", "-det.mp4"), fourcc, 20.0, (vw, vh))
 
 frames = 0
 starttime = time.time()
 static_obj = {}
-while(True):
+while True:
     ret, frame = vid.read()
     if not ret:
         break
@@ -103,34 +100,37 @@ while(True):
             cls = classes[int(cls_pred)]
             if STATIC_DETECT:
                 sum_cor = int(np.sum([x1, y1, x2, y2]))
-                print(sum_cor)
-                print(int(obj_id))
-                print(int(obj_id) in static_obj)
                 if int(obj_id) in static_obj:
                     odds = np.fabs(static_obj[int(obj_id)]["sum"] - sum_cor)
-                    print(odds)
                     if odds < 50:
-                        if static_obj[int(obj_id)]["time"] > 200 and static_obj[int(obj_id)]["time"] < 700:
-                            cv2.rectangle(frame, (x1, y1), (x1+box_w, y1+box_h), color, 4)
-                            cv2.rectangle(frame, (x1, y1-35), (x1+len(cls)*19+80, y1), color, -1)
-                            cv2.putText(frame, "STATIC " + cls + "-" + str(int(obj_id)), (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 3)
+                        if 200 < static_obj[int(obj_id)]["time"] < 700:
+                            cv2.rectangle(frame, (x1, y1),
+                                          (x1 + box_w, y1 + box_h), color, 4)
+                            cv2.rectangle(
+                                frame, (x1, y1 - 35), (x1 + len(cls) * 19 + 80, y1), color, -1)
+                            cv2.putText(frame, "STATIC " + cls + "-" + str(int(obj_id)), (x1, y1 - 10),
+                                        cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 3)
                         if static_obj[int(obj_id)]["time"] > 700:
                             second = static_obj[int(obj_id)]["time"] - 700
-                            cv2.rectangle(frame, (x1, y1), (x1+box_w, y1+box_h), colors[2], 4)
-                            cv2.rectangle(frame, (x1, y1-35), (x1+len(cls)*19+90, y1), colors[2], -1)
-                            cv2.putText(frame, "ATTENTION " + str(second) + "s", (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 3)
+                            cv2.rectangle(
+                                frame, (x1, y1), (x1 + box_w, y1 + box_h), colors[2], 4)
+                            cv2.rectangle(
+                                frame, (x1, y1 - 35), (x1 + len(cls) * 19 + 90, y1), colors[2], -1)
+                            cv2.putText(frame, "ATTENTION " + str(second) + "s", (x1, y1 - 10),
+                                        cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 3)
                         else:
                             static_obj[int(obj_id)]["time"] += 1
-                            print(static_obj[int(obj_id)]["time"])
                     else:
-                        static_obj[int(obj_id)] = {"sum": sum_cor, "time": 0 }
+                        static_obj[int(obj_id)] = {"sum": sum_cor, "time": 0}
                 else:
-                    static_obj[int(obj_id)] = {"sum": sum_cor, "time": 0 }
-                    print(static_obj)
+                    static_obj[int(obj_id)] = {"sum": sum_cor, "time": 0}
             else:
-                cv2.rectangle(frame, (x1, y1), (x1+box_w, y1+box_h), color, 4)
-                cv2.rectangle(frame, (x1, y1-35), (x1+len(cls)*19+80, y1), color, -1)
-                cv2.putText(frame, cls + "-" + str(int(obj_id)), (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 3)
+                cv2.rectangle(frame, (x1, y1),
+                              (x1 + box_w, y1 + box_h), color, 4)
+                cv2.rectangle(frame, (x1, y1 - 35),
+                              (x1 + len(cls) * 19 + 80, y1), color, -1)
+                cv2.putText(frame, cls + "-" + str(int(obj_id)), (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 1,
+                            (255, 255, 255), 3)
 
     cv2.imshow('Stream', frame)
     outvideo.write(frame)
@@ -138,7 +138,7 @@ while(True):
     if ch == 27:
         break
 
-totaltime = time.time()-starttime
-print(frames, "frames", totaltime/frames, "s/frame")
+totaltime = time.time() - starttime
+print(frames, "frames", totaltime / frames, "s/frame")
 cv2.destroyAllWindows()
 outvideo.release()
